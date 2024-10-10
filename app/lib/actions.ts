6c1d59dfe684ebb16e7b33ae74685ae71a0975b2
Oyesola Ogundele <oyekunmi@gmail.com>
2024-10-10 08:36:39 +0400

    feat: conclude accessiblity

---
+++ app/lib/actions.ts
@@ -0,0 +1,124 @@
+'use server';
+
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
+    date: z.string(),
+});
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    console.log(validatedFields);
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const amountInCents = validatedFields.data.amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    try {
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${validatedFields.data.customerId}, ${amountInCents}, ${validatedFields.data.status}, ${date})
+    `;
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return {
+                message: 'Database Error: Failed to Create Invoice.' + error.message,
+            };
+        } else {
+            return {
+                message: 'Database Error: Failed to Create Invoice. Unknown error occurred.',
+            };
+        }
+    }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        id: formData.get('id'),
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return {
+                message: 'Database Error: Failed to Create Invoice.' + error.message,
+            };
+        } else {
+            return {
+                message: 'Database Error: Failed to Create Invoice. Unknown error occurred.',
+            };
+        }
+    }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return {
+                message: 'Database Error: Failed to Create Invoice.' + error.message,
+            };
+        } else {
+            return {
+                message: 'Database Error: Failed to Create Invoice. Unknown error occurred.',
+            };
+        }
+    }
+    revalidatePath('/dashboard/invoices');
+}
